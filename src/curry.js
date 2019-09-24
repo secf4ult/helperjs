f0b27e9c9a9a6b4986e23f66fd93ffae26fb1a23
Chris Shaw <secf4ult@outlook.com>
2019-09-24 21:57:01 +0800

    Update curry.js
    Add credit

--- src/curry.js
+++ src/curry.js
@@ -1,6 +1,7 @@
 // Curry returns a copy of the invoking function, with its first n 
 // arguments pre-assigned with the arguments passed by the curry 
 // invocation.
+// Credit: https://javascriptweblog.wordpress.com/2010/04/05/curry-cooking-up-tastier-functions/
 Function.prototype.curry = function(...curryArgs) {
   // nothing to curry with
   if (curryArgs.length < 1) {
